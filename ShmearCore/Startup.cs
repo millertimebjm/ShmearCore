d25db911f4d2fd50ea4c1c2c0f5ee4c8092681e0
miller <miller@DESKTOP-0S0QS0K>
2021-03-08 22:34:06 -0600

    Fix services.AddSession() in ShmearCore

--- ShmearCore/Startup.cs
+++ ShmearCore/Startup.cs
@@ -20,6 +20,7 @@ namespace Shmear.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddMvc();
             services.AddSignalR();
         }
