ec886dc55c69a596acb23524e9ad0dcde5c95070
Brandon Miller <millertimebjm@hotmail.com>
2024-01-06 22:54:11 -0600

    add build artifacts

---
+++ shmear_new_ui/build_2_publish_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build -t shmear2-ui .
+docker run -d -p 7070:80 --name shmear2-ui shmear2-ui:1.0.0
+
+rm -rf /tmp/Containers
+
+
+
+# Check if there are any dangling images
+if [ -n "$(docker images -f dangling=true -q)" ]; then
+    # Remove dangling images
+    docker rmi --force $(docker images -f "dangling=true" -q)
+else
+    echo "No dangling images found."
+fi
+
